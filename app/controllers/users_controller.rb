81299ff1c2753e722e0aeb27eb0185650bbb49ef
Samantha Carp <samantha.carp@kellogg.northwestern.edu>
2024-05-29 00:26:38 +0000

    tues commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+ 
+
+  def index
+    @users= user.all
+  end
+ 
   def new
   end
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user["id"]}"
   end
 end
