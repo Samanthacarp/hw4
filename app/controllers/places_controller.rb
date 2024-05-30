2855948e49eeb36bb0373b906447b1eef3febda5
Samantha Carp <samantha.carp@kellogg.northwestern.edu>
2024-05-30 21:26:19 +0000

    thur edits

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,4 +19,31 @@ class PlacesController < ApplicationController
     redirect_to "/places"
   end
 
+  def edit
+    @place = Place.find_by({ "id" => params["id"] })
+  end
+
+def update
+  @place = Place.find_by({ "id" => params["id"] })
+  if User.find_by({ "id" => session["user_id"] }) != nil
+      @place["name"] = params["name"]
+      @place.save
+    else
+      flash["notice"] = "You must be logged in."
+    end
+    redirect_to "/places/#{@place["id"]}"
+  end
+
+  def destroy
+    @place = Place.find_by({ "id" => params["id"] })
+
+    if User.find_by({ "id" => session["user_id"] }) != nil
+      @place.destroy
+      redirect_to "/places"
+    else
+      flash["notice"] = "You must be logged in."
+      redirect_to "/places/#{@place["id"]}"
+    end
+  end
+
 end
